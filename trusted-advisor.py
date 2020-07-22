1857e5c2a5899293fa84ae7860910111f5f4a503
Ricardo Aldao <ricardo.aldao@gmail.com>
2020-07-22 16:39:04 +0200

    Update trusted-advisor.py

--- trusted-advisor.py
+++ trusted-advisor.py
@@ -11,31 +11,35 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-## Support API only works from us-east-1, so initializing client with that specific region
-client = boto3.client('support', region_name='us-east-1')
+try:
+    ## Support API only works from us-east-1, so initializing client with that specific region
+    client = boto3.client('support', region_name='us-east-1')
 
-## How many checks do we have? What are their names?
-response = client.describe_trusted_advisor_checks(
-    language='en'
-)
+    ## How many checks do we have? What are their names?
+    response = client.describe_trusted_advisor_checks(
+        language='en'
+    )
 
-number_checks = len(response["checks"])
-print("Found " + str(number_checks) + " active checks:")
-for i in range(0,number_checks):
-    check = response['checks'][i]
-    ## Check each check and get its status
-    check_result = client.describe_trusted_advisor_check_result(
-        checkId=check['id'],
-        language='en')
-    status = check_result['result']['status']
+    number_checks = len(response["checks"])
+    print("Found " + str(number_checks) + " active checks:")
+    for i in range(0,number_checks):
+        check = response['checks'][i]
+        ## Check each check and get its status
+        check_result = client.describe_trusted_advisor_check_result(
+            checkId=check['id'],
+            language='en')
+        status = check_result['result']['status']
 
-    ## Use some colors for each status code
-    color = bcolors.OKBLUE
-    if(status == "ok"):
-        color = bcolors.OKGREEN
-    elif(status=="warning"):
-        color = bcolors.WARNING
-    elif(status=="error"):
-        color = bcolors.FAIL
-    print( color + check['name'] + " (" + status + ")" )
-    ##for k,v in response['checks'][i].items():
+        ## Use some colors for each status code
+        color = bcolors.OKBLUE
+        if(status == "ok"):
+            color = bcolors.OKGREEN
+        elif(status=="warning"):
+            color = bcolors.WARNING
+        elif(status=="error"):
+            color = bcolors.FAIL
+        print( color + check['name'] + " (" + status + ")" )
+        ##for k,v in response['checks'][i].items():
+except Exception as error:
+    print('Error: ', error)
+    
